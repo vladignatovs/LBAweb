b4471da48021f4c8617454116898e7f6ebd9b69e
Vladislavs Ignatovs <vi00242@rvt.lv>
2024-10-01 14:00:31 +0300

    register form design update

---
+++ account.js
@@ -0,0 +1,9 @@
+function openregister() {
+    document.getElementById('register').style.visibility = 'visible';
+    document.getElementById('login').style.visibility = 'hidden';
+}
+
+function openlogin() {
+    document.getElementById('login').style.visibility = 'visible';
+    document.getElementById('register').style.visibility = 'hidden';
+}
