33f4bd0a89a4d3c03939b4fed7d899c43cab5b53
Vladislavs Ignatovs <vi00242@rvt.lv>
2024-10-07 15:05:59 +0300

    polished register/login buttons

--- account.js
+++ account.js
@@ -1,9 +1,29 @@
-function openregister() {
-    document.getElementById('register').style.visibility = 'visible';
-    document.getElementById('login').style.visibility = 'hidden';
-}
+const registerRadio = document.getElementById('REGISTER');
+const loginRadio = document.getElementById('LOGIN');
 
-function openlogin() {
-    document.getElementById('login').style.visibility = 'visible';
-    document.getElementById('register').style.visibility = 'hidden';
-}
+registerRadio.addEventListener('click', function() {
+    var registerPanel = document.getElementById('register');
+    var loginPanel = document.getElementById('login');
+
+    if(registerPanel.style.display === "none" || registerPanel.style.display === "")
+        registerPanel.style.display = "flex";
+    else {
+        if(this.checked) this.checked = false;
+        registerPanel.style.display = "none";
+    }
+    loginPanel.style.display = "none";
+});
+
+loginRadio.addEventListener('click', function() {
+    var registerPanel = document.getElementById('register');
+    var loginPanel = document.getElementById('login');
+
+    if(loginPanel.style.display === "none" || loginPanel.style.display === "")
+        loginPanel.style.display = "flex";
+    else {
+        if(this.checked) this.checked = false;
+            loginPanel.style.display = "none";
+    }
+
+    registerPanel.style.display = "none";
+});
