a1281ab6fc66611c95ccedd10a49874e3877804e
vladignatovs <vi00242@rvt.lv>
2025-01-23 22:28:14 +0200

    tailwind, accountpage fix, little fixes and todo

--- LBAweb/src/views/HomeView.vue
+++ LBAweb/src/views/HomeView.vue
@@ -1,130 +1,116 @@
-<script setup>
-</script>
-
 <template>
-    <aside>
-        <p> Basically the main point of this page, where useful redirects will be more noticable, could also be a good place to put most important socials
-            like a discord server or a redirect to the steam page of the game.
-        </p>
-        <div class="cards-page">
-            <a href="https://discord.com" class="clickable-card"><div class="card">
-                <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg" alt="">
-                <br><p> Join our official discord server, as it is our main 
-                    source of recieving feedback from our YOU!</p>
-            </div></a>
-            <a href="https://store.steampowered.com" class="clickable-card"><div class="card">
-                <img src="https://cdn.icon-icons.com/icons2/3261/PNG/512/steam_logo_icon_206670.png" alt="">
-                <br><p> Wishlist our game on steam NOW! Don't forget to let us know about your experience! </p>
-            </div></a>
-            <a href="account.html" class="clickable-card"><div class="card">
-                <img src="@/assets/20241009_191656.png" alt="">
-                <br><p> Create an account for further usage in both our GAME and WEBSITE!</p>
-            </div></a>
-        </div>
-    </aside>
-    <main>
-        <section id="about">
-            <p> Bunch of info about the game, history of development as well as some other details, socials (?) and useful links</p>
-        </section>
-    </main>
+  <aside class="grid gap-12 py-12 mx-0 text-xl text-center bg-[var(--background)] text-white">
+    <p class="px-12">
+      Basically the main point of this page, where useful redirects will be more noticable, could
+      also be a good place to put most important socials like a discord server or a redirect to the
+      steam page of the game.
+    </p>
+    <div class="cards-page">
+      <a href="https://discord.com" class="clickable-card">
+        <div class="card">
+          <img
+            src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg"
+            alt=""
+          />
+          <br />
+          <p>
+            Join our official discord server, as it is our main source of recieving feedback from
+            our YOU!
+          </p>
+        </div></a
+      >
+      <a href="https://store.steampowered.com" class="clickable-card"
+        ><div class="card">
+          <img src="../assets/steam_logo_icon.webp" alt="" />
+          <br />
+          <p>Wishlist our game on steam NOW! Don't forget to let us know about your experience!</p>
+        </div></a
+      >
+      <a href="account.html" class="clickable-card"
+        ><div class="card">
+          <img src="../assets/account_icon.webp" alt="" />
+          <br />
+          <p>Create an account for further usage in both our GAME and WEBSITE!</p>
+        </div></a
+      >
+    </div>
+  </aside>
+  <!-- TO BE CHANGED -->
+  <main class="h-[5000px] bg-[var(--background)] text-[var(--text)]">
+    <section id="about" class="font-bold">
+      <p>
+        Bunch of info about the game, history of development as well as some other details, socials
+        (?) and useful links
+      </p>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-aside {
-    margin: 50px 0;
-
-    display: grid;
-    gap: 50px;
-    font-size: large;
-
-    text-align: center;
-}
-
-aside p {
-    padding: 0 50px;
-}
-
-main {
-    height: 5000px;
-}
-
-footer  {
-    background-color: black;
-    color: gray;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 75px;
-    font-size: x-large;
-}
-
-
+/* TODO FIX STYLES, FIX ANIMATIONS */
+/* ALSO FIX COLOR SCHEME, FIND HOW TO USE IT */
 .cards-page {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
 }
 
 @media (max-width: 600px) {
-    .cards-page {
-        flex-direction: column;
-        gap: 30px;
-    }
-    .cards-page a {
-        width: 90%;
-    }
+  .cards-page {
+    flex-direction: column;
+    gap: 30px;
+  }
+  .cards-page a {
+    width: 90%;
+  }
 }
 
 img {
-    width: 75px;
-    height: 75px;
+  width: 75px;
+  height: 75px;
 
-    filter: invert(1);
-    transition-duration: .5s;
-
+  filter: invert(1);
+  transition-duration: 0.5s ease-in;
 }
 
 .card {
-    height: inherit;
-    border-radius: 15px;
-    min-height: 300px;
-    display: flex;
-    flex-direction: column;
+  height: inherit;
+  border-radius: 15px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
 
-    justify-content: center;
-    align-items: center;
+  justify-content: center;
+  align-items: center;
 
-    text-align: center;
+  text-align: center;
 
-    background-color: black;
-    background-image: linear-gradient(black, black, rgba(21, 0, 255, 0.25));
-    color: white;
+  background-image: linear-gradient(var(--primary), var(--primary), var(--primary));
+  color: var(--text);
 
-    transition-duration: .5s;
+  transition-duration: 0.5s;
 }
 
-
 .card p {
-    padding: 0 30px;
+  padding: 0 30px;
 }
 
 .card:hover {
-    transform: scale(1.05);
-    z-index: 1;
-    background-image: linear-gradient(white, white, rgba(21, 0, 255, 0.25));
-    background-color: white;
-    color: black;
+  transform: scale(1.05);
+  z-index: 1;
+  transition: ease-out;
+  background-image: linear-gradient(var(--secondary), var(--secondary), var(--secondary));
+  color: black;
 }
 
 .card:hover img {
-    filter: invert(0);
+  filter: invert(0);
 }
 .clickable-card {
-    height: inherit;
-    width: 30%;
+  height: inherit;
+  width: 30%;
 
-    text-decoration: none;
+  text-decoration: none;
 }
 </style>
--- LBAweb/vite.config.js
+++ LBAweb/vite.config.js
@@ -1,21 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   server: {
-    port: 8080
+    port: 8080,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
