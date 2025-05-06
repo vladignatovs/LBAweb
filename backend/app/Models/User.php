f8d9c83471ff6f9b0160ef659370fd032f4dcdf6
vladignatovs <vi00242@rvt.lv>
2025-05-06 21:57:28 +0300

    small user.php fix

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -47,4 +47,58 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    // friend requests I’ve sent
+    public function friendRequestsSent()
+    {
+        return $this->hasMany(FriendRequest::class, 'sender_id');
+    }
+
+    // friend requests I’ve received
+    public function friendRequestsReceived()
+    {
+        return $this->hasMany(FriendRequest::class, 'receiver_id');
+    }
+
+    // my friends (bidirectional pivot)
+    public function friends()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'friendships',
+            'friend_id',
+            'friended_id'
+        );
+    }
+
+    // blocks I’ve made
+    public function blocks()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'blocks',
+            'blocker_id',
+            'blocked_id'
+        );
+    }
+
+    // messages I’ve sent
+    public function messagesSent()
+    {
+        return $this->hasMany(Message::class, 'sender_id');
+    }
+
+    // messages I’ve received
+    public function messagesReceived()
+    {
+        return $this->hasMany(Message::class, 'receiver_id');
+    }
+
+    // levels I can see
+    public function levels()
+    {
+        return $this->belongsToMany(Level::class, 'completions')
+                    ->withTimestamps();
+    }
 }
